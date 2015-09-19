9775396e7ad26710837238cc73ed41858a1590e4
Devon Koch <devon.r.koch@gmail.com>
2015-09-18 17:14:51 -0700

    Help Request System Complete
    in studentProjectView,
      question button with font-awesome question icon
        opening it prompts a modal
      when the student submits a question, a socket triggers two events:
    
    1. teacherClass --> top-right dropdown notifiction with student name and question
    2. helpRequest --> New Request table is updated in real time
    
    in helpRequestView
      when a helpRequest in the new request table is clicked,
        it moves the question to the acknowledged table in the view
           toggles the acknowledged column in the db to true
           triggers a socket that sends a student in the project view a notification that the teacher has seen the request

--- client/app/navbar/navbarCtrl.js
+++ client/app/navbar/navbarCtrl.js
@@ -1,8 +1,10 @@
 angular.module('app')
-  .controller('navbarCtrl', function($scope, $state, $http, $rootScope, User) {
+  .controller('navbarCtrl', function ($scope, $state, $http, $rootScope, User, HelpRequest) {
     
     $scope.navBarCollapsed = true;    
     $scope.loggedIn = false;
+    $scope.helpRequestTotal;
+
 
     $scope.logout = function() {
       $http.post('/auth/logout', {}).then(function(response) {
@@ -18,10 +20,14 @@ angular.module('app')
 
     // makes sure the nav bar only shows the appropriate links for the user
     var setNavbar = function() {
+
       var accountType = User.getUser().accountType;
       $scope.loggedIn = true;
       $scope.isTeacher = accountType === 'Teacher' ? true : false;
+
     };
+
+
 
     $rootScope.$on('teacherOrStudent', setNavbar);
 });
